e0d9837c4ddaa76ceeca7a8a8c50bc12a5a9f754
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2025-01-27 01:49:31 -0300

    Adjusted execution of current tasks on cloud_aws_terraform workflow

--- cloud-aws-terraform/terraform/state.tf
+++ cloud-aws-terraform/terraform/state.tf
@@ -1,8 +1,4 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-workload-data"
-    region         = "us-east-1"
-    profile        = "remote-state"
-    dynamodb_table = "terraform-workload-lock"
   }
 }
