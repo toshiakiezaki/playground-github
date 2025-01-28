6deed811e4097cbe06970a25a5cd4ffeca89bdbb
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2025-01-28 01:19:36 -0300

    Added resource for testing

--- cloud-aws-terraform/terraform/variables.tf
+++ cloud-aws-terraform/terraform/variables.tf
@@ -3,9 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "aws_profile" {
-  description = "AWS profile"
-  type        = string
-  default     = "default"
-}
