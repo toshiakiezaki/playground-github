56dd09a1ebe3373b832c98262e50dc9e0bc55a5f
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2025-10-31 01:41:54 -0300

    Created initial terraform files

---
+++ cloud-gcp-terraform/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_project" {
+  description = "GCP project"
+  type        = string
+  default     = "playground-20240117"
+}
+
+variable "gcp_region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-east1"
+}
